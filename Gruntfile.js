dc23955b25931256958bf258f5195f6c8fc6e0d9
Matt Glaman <nmd.matt@gmail.com>
2015-01-02 19:37:49 -0600

    Fix builds - vendor was not compiling. Need to investigate build task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,6 +257,7 @@ module.exports = function (grunt) {
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*',
             '_locales/{,*/}*.json',
+            'vendor/*/**'
           ]
         }]
       },
